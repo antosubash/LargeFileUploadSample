b73fbbfc3791ebb648083c7a667e36be25d9b47a
Anto Subash <antosubash@live.com>
2021-07-25 22:11:33 +0200

    Update Startup.cs

--- dotnet/Startup.cs
+++ dotnet/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
 using tusdotnet;
 using tusdotnet.Helpers;
 using tusdotnet.Interfaces;
